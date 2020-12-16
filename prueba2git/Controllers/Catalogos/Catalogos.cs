fe03d28ff67e41e8947b4c3838d4619c8c710070
jlchdz00 <jose_luis@cjslp.gob.mx>
2020-12-16 12:58:14 -0600

    Se anexa en controlador los catalogos

---
+++ prueba2git/Controllers/Catalogos/Catalogos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prueba2git.Controllers.Catalogos
+{
+    public class Catalogos
+    {
+    }
+}
